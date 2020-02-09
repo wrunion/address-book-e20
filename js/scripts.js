4539a549023c1ef0606bceadf77a123b11aa344c
Student <example@example.com>
2020-02-09 12:28:29 -0800

    Initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+    this.contacts = [];
+    this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+    for (let i = 0; i < this.contacts.length; i++) {
+        if (this.contacts[i].id == id) {
+            return this.contacts[i];
+        }
+    };
+    return false;
+}
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+}
+
+
+Contact.prototype.fullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+
+//test functions
+let testBook = new AddressBook();
+var testContact = new Contact("Ada","Lovelace","503-555-1111");
+var testContact2 = new Contact("Ada","Lovelace","503-555-1111");
+// ​var testContact2 = new Contact("Ada","Lovelace","503-555-1111");
+// ​var testContact2 = new Contact("Ada","Lovelace","503-555-1111");
+// ​
