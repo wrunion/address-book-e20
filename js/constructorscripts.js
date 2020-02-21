d5c77701eb66c916f6ca1c1948b1612898939dfa
Winter Runion <winterrunion@gmail.com>
2020-02-20 18:37:50 -0800

    Refactor constructors into classes.

--- js/constructorscripts.js
+++ js/constructorscripts.js
@@ -1,7 +1,12 @@
-// Business Logic: AddressBook -------------------------------
+// Business Logic: Constructors and Protypes (pre-ES6 version) -------------------------
 function AddressBook() {
   this.contacts = [];
   this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
 }
 
 AddressBook.prototype.assignId = function() {
@@ -10,34 +15,25 @@ AddressBook.prototype.assignId = function() {
 }
 
 AddressBook.prototype.findContact = function(id) {
-  for (let i=0; i<this.contacts.length; i++) {
-    if (this.contacts[i]) {
-      if (this.contacts[i].id === id) {
-        return this.contacts[i];
+  for (let i = 0; i < this.contacts.length; i++) {
+      if (this.contacts[i].id == id) {
+          return this.contacts[i];
       }
-    }
   };
   return false;
 }
 
 AddressBook.prototype.deleteContact = function(id) {
   for (let i=0; i<this.contacts.length; i++) {
-    if (this.contacts[i]) {
       if (this.contacts[i].id === id) {
-        delete this.contacts[i];
-        return true;
+          delete this.contacts[i];
+          return true;
       }
-    }
   };
   return false;
 }
 
-AddressBook.prototype.addContact = function(contact) {
-  contact.id = this.assignId();
-  this.contacts.push(contact);
-}
-
-// Business Logic: Contacts ----------------------------------
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -47,6 +43,7 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return `${this.firstName} ${this.lastName}`;
 }
+
 
 //UI Logic ----------------------------------------------------
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,121 @@
-// Business Logic for AddressBook ---------
-function AddressBook() {
+// Business Logic: ES6 ---------
+
+// Address Book
+class AddressBook {
+  constructor(name) {
+    this.name = name;
     this.contacts = [];
     this.currentId = 0;
-}
-
-AddressBook.prototype.addContact = function(contact) {
+  }
+  assignId() {
+    this.currentId +=1;
+    return this.currentId;
+  }
+  addContact(contact) {
     contact.id = this.assignId();
     this.contacts.push(contact);
-}
-
-AddressBook.prototype.assignId = function() {
-    this.currentId += 1;
-    return this.currentId;
-}
-
-AddressBook.prototype.findContact = function(id) {
-    for (let i = 0; i < this.contacts.length; i++) {
-        if (this.contacts[i].id == id) {
+  }
+  findContact(id) {
+    for (let i=0; i<this.contacts.length; i++) {
+      if (this.contacts[i]) {
+        if (this.contacts[i].id === id) {
             return this.contacts[i];
         }
+      }
     };
     return false;
+  }
+  deleteContact(id) {
+    for (let i=0; i<this.contacts.length; i++) {
+      if (this.contacts[i]) {
+        if (this.contacts[i].id === id) {
+            delete this.contacts[i];            
+          }
+        }
+      };
+    return false;
+  }
 }
 
-// Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
-}
-
-
-Contact.prototype.fullName = function() {
+// Contacts
+class Contact {
+  constructor(firstName, lastName, phoneNumber) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.phoneNumber = phoneNumber;
+  }
+  fullName() {
     return `${this.firstName} ${this.lastName}`;
+  }
 }
 
+// UI Logic ------------------------
 
-//test functions
-let testBook = new AddressBook();
-var testContact = new Contact("Ada","Lovelace","503-555-1111");
-var testContact2 = new Contact("Ada","Lovelace","503-555-1111");
+let wintersAddressBook = new AddressBook();
 
-
-// ------------------ User Interface Logic ------------------
-
-var wintersAddressBook = new AddressBook();
-
-function displayContactDetails(addressBookToDisplay) {
-    //debugger;
-    let contactsList = $("ul#contacts");
-    let htmlForContactInfo= "";
-    addressBookToDisplay.contacts.forEach(function(contact) {
-        htmlForContactInfo += `<li id=${contact.id}> ${contact.firstName} ${contact.lastName} </li>`;
-    });
-    contactsList.html(htmlForContactInfo);
-}
-
-function displayPhoneNumber(addressBookToDisplay) {
-    let contactsList = $("ul#phoneNumbers");
-    let htmlForPhoneNumber = "";
-    addressBookToDisplay.contacts.forEach(function(contact) {
-        htmlForPhoneNumber += `<li id=${contact.id}> ${contact.phoneNumber} </li>`;
-    });
-    contactsList.html(htmlForPhoneNumber);
+function displayContacts(bookName) {
+let display = $("ul#contacts");
+let displayHTML = "";
+bookName.contacts.forEach(function(contact) {
+  displayHTML += `<li id=${contact.id}>${contact.firstName} ${contact.lastName}</li> <button class="displayAll">Show Details</button> <button class="deleteContact">Delete</button>`;
+});
+display.html(displayHTML);
 }
 
 function attachContactListeners() {
-    $("ul#contacts").on("click", "li", function() {
-        console.log("The id of this <li> is " + this.id + ".");
-    });
+  $("ul#contacts").on("click", "li", function() {
+      console.log("The id of this <li> is " + this.id + ".");
+  });
 };
 
-// function attachContactListeners() {
-//     $("ul#contacts").on("click", "li", function() {
-//         console.log(`The id of this <li> is ${this.id}.`);
-//     });
-// };
 
 $(document).ready(function() {
-    attachContactListeners();
+  attachContactListeners();
 
-    $("form#new-contact").submit(function(event) {
-        event.preventDefault();
-        var inputtedFirstName = $("input#firstName").val();
-        var inputtedLastName = $("input#lastName").val();
-        var inputtedPhoneNumber = $("input#phoneNumber").val();
-        var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
-        wintersAddressBook.addContact(newContact);
-        displayContactDetails(wintersAddressBook);
-        displayPhoneNumber(wintersAddressBook);
-    });
+  $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+      // Grab input from user
+      let firstName = $("#firstName").val();
+      let lastName = $("#lastName").val();
+      let phoneNumber = $("#phoneNumber").val()
+
+      // Create new contact & push it to the working address book
+      let newContact = new Contact(firstName, lastName, phoneNumber);
+      wintersAddressBook.addContact(newContact);
+      displayContacts(wintersAddressBook);
+  });
 
 });
+
+//product needs discovery phase
+
+// $(document).ready(function() {
+//     attachContactListeners();
+//     //Ask the user for their name and create a new AddressBook object unique to them. Append the "Conacts" display header with their name as well
+//     //let userName = prompt(`To create a new address book, please enter your first name:`);
+
+//     //Create a placeholder username for testing
+//     let userName = "Winter";
+//     $("span#user-name").append(`${userName}'s `);
+
+//     //NOTE: the below code doesn't work. come back to it.
+//     //let bookname = userName.toLowerCase() + "sAddressBook";
+//     // let newBookName = `${userName.toLowerCase()}sAddressBook`;
+//     // this[newBookName] = new AddressBook;
+//     $("form#new-contact").submit(function(event) {
+//       let firstName = $("#firstName").val();
+//       //console.log(firstName);
+//       let lastName = $("#lastName").val();
+//       //console.log(lastName);
+//       let phone = $("#phoneNumber").val();
+//       //console.log(phone);
+
+//       let currentContact = new Contact(firstName, lastName, phone);
+
+//       currentBook.addContact(currentContact);
+//       console.log(currentBook);
+//       displayContacts(currentBook);
+//       event.preventDefault();
+//     });
+
+//   });
